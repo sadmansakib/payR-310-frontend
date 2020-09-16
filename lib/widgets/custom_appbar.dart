07d94a7239de12cd7ab8283bbd8417c19a5a1d5a
Sadman Sakib <ssadman8@gmail.com>
2020-09-16 20:57:41 +0600

    fix optional button paratemer for nav bar

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -26,7 +26,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
             ),
           ),
           Spacer(),
-          appBarButton,
+          appBarButton != null ? appBarButton : Container()
         ],
       ),
     );
